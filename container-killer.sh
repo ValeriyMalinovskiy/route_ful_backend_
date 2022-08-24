9fa8df03e6e48576e0443c7c186e65de7b46b497
Trotskovets Alexey <a.trotskovets@mobidev.biz>
2022-08-24 21:30:14 +0300

    script has been added

---
+++ container-killer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Checking if the container $1  exist"
+
+if [ "$(docker ps -q -f name=$1)" ]; then
+    # stop your container
+    echo "Container $1 is running now. Trying to stop"
+    docker stop $1
+fi
