680a52dbcd9468f58697eff3491edc443cce2048
Trotskovets Alexey <a.trotskovets@mobidev.biz>
2022-08-24 23:12:25 +0300

    remove option added

--- container-killer.sh
+++ container-killer.sh
@@ -2,7 +2,7 @@
 
 echo "Checking if the container $1  exist"
 
-if [ "$(docker ps -q -f name=$1)" ]; then
+if [ "$(docker ps -q -a -f name=$1)" ]; then
     # stop your container
     echo "Container $1 is running now. Trying to stop"
     docker rm --force $1
