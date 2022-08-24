a27c6934e7196b885552ffafe252b8438986584b
Trotskovets Alexey <a.trotskovets@mobidev.biz>
2022-08-24 22:55:40 +0300

    has been removed

--- container-killer.sh
+++ container-killer.sh
@@ -5,5 +5,5 @@ echo "Checking if the container $1  exist"
 if [ "$(docker ps -q -f name=$1)" ]; then
     # stop your container
     echo "Container $1 is running now. Trying to stop"
-    docker stop $1
+    docker rm --force $1
 fi
