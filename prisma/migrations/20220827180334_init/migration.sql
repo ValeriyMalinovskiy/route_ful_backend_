2bfe363810271766ae12d0611802c7513aefeb0f
Valerii Malynovskyi <mdfcnvsn@gmail.com>
2022-08-27 21:05:36 +0300

    fixing schema

--- prisma/migrations/20220827180334_init/migration.sql
+++ prisma/migrations/20220827180334_init/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE `User` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
     `joinDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-    `userName` VARCHAR(255) NOT NULL,
+    `userName` VARCHAR(191) NOT NULL,
     `userPhoto` VARCHAR(191) NULL,
 
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -14,7 +16,7 @@ CREATE TABLE `Activity` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `private` BOOLEAN NOT NULL DEFAULT false,
     `typeId` INTEGER NOT NULL,
-    `userId` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
     `trackId` INTEGER NOT NULL,
 
     UNIQUE INDEX `Activity_typeId_key`(`typeId`),
@@ -34,18 +36,17 @@ CREATE TABLE `ActivityType` (
 CREATE TABLE `Track` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `geoData` VARCHAR(191) NOT NULL,
-    `activityId` INTEGER NOT NULL,
 
-    UNIQUE INDEX `Track_geoData_key`(`geoData`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Follower` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `userId` VARCHAR(191) NOT NULL,
+CREATE TABLE `_UserFollows` (
+    `A` INTEGER NOT NULL,
+    `B` INTEGER NOT NULL,
 
-    PRIMARY KEY (`id`)
+    UNIQUE INDEX `_UserFollows_AB_unique`(`A`, `B`),
+    INDEX `_UserFollows_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
@@ -55,7 +56,10 @@ ALTER TABLE `Activity` ADD CONSTRAINT `Activity_typeId_fkey` FOREIGN KEY (`typeI
 ALTER TABLE `Activity` ADD CONSTRAINT `Activity_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Activity` ADD CONSTRAINT `Activity_trackId_fkey` FOREIGN KEY (`trackId`) REFERENCES `Track`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Track` ADD CONSTRAINT `Track_id_fkey` FOREIGN KEY (`id`) REFERENCES `Activity`(`trackId`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Follower` ADD CONSTRAINT `Follower_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `_UserFollows` ADD CONSTRAINT `_UserFollows_A_fkey` FOREIGN KEY (`A`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_UserFollows` ADD CONSTRAINT `_UserFollows_B_fkey` FOREIGN KEY (`B`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
