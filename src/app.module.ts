9a4e4ceb1d4239f39260e0d6dd8f70226f868fca
Valerii Malynovskyi <mdfcnvsn@gmail.com>
2022-08-28 19:12:15 +0300

    first steps

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [TestModule],
+  imports: [
+    TestModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
